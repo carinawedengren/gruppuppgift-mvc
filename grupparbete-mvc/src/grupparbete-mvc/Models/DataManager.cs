fb92b298754265f7aa7181e40465d3d200f645c1
Carina Wedengren <carina.wedengren@gmail.com>
2016-07-26 10:35:37 +0200

    more tracks...

--- grupparbete-mvc/src/grupparbete-mvc/Models/DataManager.cs
+++ grupparbete-mvc/src/grupparbete-mvc/Models/DataManager.cs
@@ -55,11 +55,44 @@ namespace grupparbete_mvc.Models
             la.Add(a);
 
             a = new Album { Title = "Animals", Year = 1977 };
-            a.Tracks = new List<Track> { };
+            a.Tracks = new List<Track> {
+                new Track { Name="Pigs on the Wing 1", Length=new TimeSpan(0,1,25)},
+                new Track { Name="Dogs", Length=new TimeSpan(0,17,03)},
+                new Track { Name="Pigs (Three Different Ones)", Length=new TimeSpan(0,11,25)},
+                new Track { Name="Sheep", Length=new TimeSpan(0,10,25)},
+                new Track { Name="Pigs on the Wing 2", Length=new TimeSpan(0,1,23)}
+            };
             la.Add(a);
 
             a = new Album { Title = "The Wall", Year = 1979 };
-            a.Tracks = new List<Track> { };
+            a.Tracks = new List<Track> {
+                new Track { Name="In the Flesh", Length=new TimeSpan(0,3,16)},
+                new Track { Name="The Thin  Ice", Length=new TimeSpan(0,2,27)},
+                new Track { Name="Another Brick in the Wall (Part I)", Length=new TimeSpan(0,3,21)},
+                new Track { Name="The Happiest Days of Our Lives", Length=new TimeSpan(0,1,46)},
+                new Track { Name="Another Brick in the Wall (Part II)", Length=new TimeSpan(0,3,59)},
+                new Track { Name="Mother", Length=new TimeSpan(0,5,32)},
+                new Track { Name="Goodbye Blue Sky", Length=new TimeSpan(0,2,45)},
+                new Track { Name="Empty Spaces", Length=new TimeSpan(0,2,10)},
+                new Track { Name="Young Lust", Length=new TimeSpan(0,3,25)},
+                new Track { Name="One of My Turns", Length=new TimeSpan(0,3,41)},
+                new Track { Name="Don't Leave Me Now", Length=new TimeSpan(0,4,08)},
+                new Track { Name="Another Brick in the Wall (Part III)", Length=new TimeSpan(0,1,48)},
+                new Track { Name="Goodbye Cruel World", Length=new TimeSpan(0,0,48)},
+                new Track { Name="Hey You", Length=new TimeSpan(0,4,40)},
+                new Track { Name="Is There Anybody Out There?", Length=new TimeSpan(0,2,44)},
+                new Track { Name="Nobody Home", Length=new TimeSpan(0,3,26)},
+                new Track { Name="Vera", Length=new TimeSpan(0,1,35)},
+                new Track { Name="Bring the Boys Back Home", Length=new TimeSpan(0,1,21)},
+                new Track { Name="Comfortably Numb", Length=new TimeSpan(0,6,23)},
+                new Track { Name="The Show Must Go On", Length=new TimeSpan(0,1,36)},
+                new Track { Name="In the Flesh", Length=new TimeSpan(0,4,15)},
+                new Track { Name="Run Like Hell", Length=new TimeSpan(0,4,20)},
+                new Track { Name="Waiting for the Worms", Length=new TimeSpan(0,4,04)},
+                new Track { Name="Stop", Length=new TimeSpan(0,0,30)},
+                new Track { Name="The Trial", Length=new TimeSpan(0,5,13)},
+                new Track { Name="Outside", Length=new TimeSpan(0,1,41)}
+        };
             la.Add(a);
         }
         public static Band GetBand()
