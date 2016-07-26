046b3607a886fbb0810f4915c6f6991af5f386cb
SamuelWr <samuel.wraak@gmail.com>
2016-07-26 09:20:22 +0200

    test

--- grupparbete-mvc/src/grupparbete-mvc/Startup.cs
+++ grupparbete-mvc/src/grupparbete-mvc/Startup.cs
@@ -16,6 +16,7 @@ namespace grupparbete_mvc
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //TESTKOMMENTAR
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
